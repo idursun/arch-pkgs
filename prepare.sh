b92d37ab99ffece33d68aeea50dfa0a2c690d908
ibrahim dursun <ibrahim.dursun@gmail.com>
2019-12-29 16:24:35 +0000

    Use arch root while building packages

--- prepare.sh
+++ prepare.sh
@@ -2,13 +2,33 @@
 
 PACKAGES=${@:-pkg/*}
 
-CURDIR=$PWD
-for package in $PACKAGES; do 
-    cd ${CURDIR}/$package
-    makepkg -s
-done
+LOCAL_PATH=$HOME/.local/share/arch-repo
+CHROOT="$HOME/.local/share/arch-root"
+REPO_NAME=idursun
 
-cd $CURDIR
+mkdir -p "$LOCAL_PATH"
+mkdir -p "$CHROOT"
+
+[[ -d "$CHROOT/root" ]] || mkarchroot -C /etc/pacman.conf "$CHROOT/root" \
+    bash bzip2 coreutils cryptsetup device-mapper dhcpcd diffutils e2fsprogs \
+    file filesystem findutils gawk gcc-libs gettext glibc grep gzip inetutils \
+    iproute2 iputils less licenses linux logrotate man-db man-pages mdadm \
+    pacman pciutils perl procps-ng psmisc s-nail sed shadow sysfsutils \
+    systemd-sysvcompat tar texinfo usbutils util-linux which base-devel
+
+for package in $PACKAGES; do (
+    cd "$package" || ( echo "No such directory: $package"; exit 1 )
+    rm -f ./*.pkg.tar.xz
+    makechrootpkg -cur "$CHROOT"
+) done
+
+#CURDIR=$PWD
+#for package in $PACKAGES; do 
+#    cd ${CURDIR}/$package
+#    makepkg -s
+#done
+#
+#cd $CURDIR
 
 #cat >>/etc/pacman.conf <<EOF
 #[idursun]
